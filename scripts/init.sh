852ba7b4f2aba596ff7063fb9d678ee873729fa4
Sherly1001 <Sherly1001@users.noreply.github.com>
2020-11-28 08:33:22 +0700

    slock

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 echo '#!/bin/sh' > ~/.xinitrc
 echo 'exec i3' >> ~/.xinitrc
-sudo cp ./battery_plug /etc/acpi/events/
-sudo cp ./headphone_jack /etc/acpi/events/
-systemctl enable betterlockscreen@$USER
+sudo cp ./scripts/battery_plug /etc/acpi/events/
+sudo cp ./scripts/headphone_jack /etc/acpi/events/
+
+sudo cp ./scripts/lock.service /etc/systemd/system/lock.service
+systemctl enable lock
